4310211093e64281108de70ea023aff0e1e9cadf
Evelyn Hernandez <hernandeze791@gmail.com>
2020-09-07 06:48:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -6,9 +6,11 @@ def load_library(file_path)
   new_hash = {}
   emoticons.each do |key|
     new_hash[key] = {}
-
+    new_hash[key][:english] = value [0]
+    new_hash[key][:japanese] = value[1]
+  end
   #binding.pry
-
+new_hash
 
   # code goes here
 end
